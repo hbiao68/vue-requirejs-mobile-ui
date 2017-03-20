cb68ccae481f77c3833f570fd8d2681b7c887ea3
heruiwoniou <heruiwoniou@qq.com>
2017-03-20 17:17:27 +0800

    '修改'

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,10 @@
         'store/modules/common/Evaluate/store',
         'store/modules/common/EvaluateClass/store'
     ].concat(__config__.map(function(o) {
-        var module = o.route.replace(/\//g, '_');
-        var func = ";define('store/modules/base/" + module + "',['__store__factory__','store/modules/" + o.path + "/store'],function(factory,storeModule){ var mb = factory('" + module + "'); var m = new storeModule('" + module + "'); var c = $.extend(true,{},mb, m);  return c; });";
+        var module = o.route.replace(/\/:[^\/]*/g,'').replace(/\//g,'_');
+        var func = (o.store == true ?
+            ";define('store/modules/base/" + module + "',['__store__factory__','store/modules/" + o.path + "/store'],function(factory,storeModule){ var mb = factory('" + module + "'); var m = new storeModule('" + module + "'); var c = $.extend(true,{},mb, m);  return c; });" :
+            ";define('store/modules/base/" + module + "',['__store__factory__'],function(factory){ return factory('" + module + "');});");
         __config__.dynamic(func);
         return 'store/modules/base/' + module;
     }));
@@ -21,7 +23,7 @@
             EvaluateClass: EvaluateClass,
         };
         __config__.each(function(o, i) {
-            modules[o.route.replace(/\//g, '_')] = m[i];
+            modules[o.route.replace(/\/:[^\/]*/g,'').replace(/\//g,'_')] = m[i];
         });
         return new Vuex.Store({
             state: {},
