31d1ece4eb901ab17680d16f59184c1b7b458aa1
heruiwoniou <hero.hrdzl@gmail.com>
2017-09-28 10:27:33 +0800

    '修改了搜索框展示方式'

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,9 @@
-define(['__component__', 'text!./tpl.html', 'components/Cell/index'], function(component, template, csCell) {
+define(['__component__', 'text!./tpl.html', 'components/Cell/index'], function (component, template, csCell) {
     return component('cs-search', {
         template: template,
-        data: function() {
+        data: function () {
             return {
-                visible: false,
+                visible: true,
                 currentValue: this.value
             };
         },
@@ -11,18 +11,31 @@ define(['__component__', 'text!./tpl.html', 'components/Cell/index'], function(c
         components: { csCell: csCell },
 
         watch: {
-            currentValue: function(val) {
+            currentValue: function (val) {
                 this.$emit('input', val);
             },
 
-            value: function(val) {
+            value: function (val) {
                 this.currentValue = val;
+            },
+
+            show: function (nv, ov) {
+                if (nv) this.$nextTick(function () {
+                    this.currentValue = ''
+                    this.autofocus && this.$refs.input.focus();
+                })
             }
         },
 
+        computed: {
+            isEmpty: function () {
+                return this.currentValue === '' || this.currentValue === undefined
+            }
+        },
         props: {
             value: String,
             autofocus: Boolean,
+            show: Boolean,
             cancelText: {
                 default: '取消'
             },
@@ -31,16 +44,9 @@ define(['__component__', 'text!./tpl.html', 'components/Cell/index'], function(c
             },
             result: Array
         },
-        watch: {
-            show: function(ov, nv) {
-                if (nv) this.$nextTick(function() {
-                    this.autofocus && this.$refs.input.focus();
-                })
-            }
-        },
         methods: {
-            cancel: function() {
-                this.visible = false;
+            cancel: function () {
+                // this.visible = false;
                 if (this.canceled) this.canceled();
             }
         }
